ad93afb92dfb73cd2c3681b276bd8944772fdccf
Jason <azayevjason@gmail.com>
2019-12-13 13:33:05 -0500

    Initial commit of readme and gitignore

---
+++ README.py
@@ -0,0 +1,13 @@
+# Neural Network for AI Course
+
+## What are we doing?
+We are following the tutorial found [here](https://medium.com/binaryandmore/beginners-guide-to-deriving-and-implementing-backpropagation-e3c1a5a1e536) about a basic introduction to Neural Networks in Python3. Our goal is to
+- Modify the code to accept CIFAR-10 data
+- Re-implement the code in C++
+- And ideally include SSE Optimizations, to speed it up even more.
+
+## Why are we doing this?
+This is both a learning experience for us, and a chance to apply optimizations to make this algorithm run even faster.
+
+## Credit
+We are basing everything initial off of [Pranav Budhwant's article](https://medium.com/binaryandmore/beginners-guide-to-deriving-and-implementing-backpropagation-e3c1a5a1e536) on neural networks. The original repository can be found [here](https://github.com/pranavbudhwant/backpropagation-in-numpy).
